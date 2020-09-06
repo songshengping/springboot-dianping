852d005287de378590b89fcefdea4a6be6cbe558
='songshengping <=>
2020-09-06 10:16:29 +0800

    first

---
+++ src/main/java/com/song/springbootdianping/SpringbootDianpingApplication.java
@@ -0,0 +1,13 @@
+package com.song.springbootdianping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDianpingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDianpingApplication.class, args);
+    }
+
+}
